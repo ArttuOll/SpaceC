5fe9ed17f2fcc8cc8e86207d21b4892494d96c83
ArttuOll <arttuoll.tech@tutanota.com>
2021-10-13 15:03:04 +0300

    Delete extra keyword

--- src/main/java/lexer/tokenTypes/Keyword.java
+++ src/main/java/lexer/tokenTypes/Keyword.java
@@ -2,5 +2,5 @@ package lexer.tokenTypes;
 
 public enum Keyword implements TokenType {
     AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+    PRINT, RETURN, SUPER, THIS, TRUE, WHILE,
 }
