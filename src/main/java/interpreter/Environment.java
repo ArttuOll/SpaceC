430890070c810573868dc3f21fd3eec160a37a2c
ArttuOll <arttuoll.tech@tutanota.com>
2021-11-19 16:27:35 +0200

    Create variable environment for storing variables

---
+++ src/main/java/interpreter/Environment.java
@@ -0,0 +1,29 @@
+package interpreter;
+
+import java.util.HashMap;
+import lexer.Token;
+import utils.PropertiesReader;
+
+public class Environment {
+
+    private final HashMap<String, Object> values = new HashMap<>();
+    private final PropertiesReader propertiesReader = new PropertiesReader(
+        "src/main/resources/strings.properties");
+
+    public void define(String name, Object value) {
+        values.put(name, value);
+    }
+
+    public Object get(Token name) {
+        String key = name.lexeme();
+        if (values.containsKey(key)) {
+            return values.get(key);
+        }
+
+        throw new RuntimeError(
+            name,
+            propertiesReader.getString("interpreter_error_undefined_variable"),
+            key
+        );
+    }
+}
